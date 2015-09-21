a03d69b50343c19cf830a8424c4a320ffb8b1f60
Lucas Lopatka <lucaslopatka@gmail.com>
2015-09-20 19:57:33 -0600

    Hasher/Crossroads dist directories were being ignored; minor block and stylesheet changes

--- blocks/block.contactinfo.js
+++ blocks/block.contactinfo.js
@@ -32,7 +32,7 @@ define({
 							type: 'text',
 							class: 'medium k-textbox',
 							data: {
-								bind: 'emailAddress'
+								bind: 'email'
 							}
 						},
 						{
@@ -61,13 +61,13 @@ define({
 							type: 'number',
 							class: 'medium k-textbox',
 							data: {
-								bind: 'homePhone'
+								bind: 'telephone'
 							},
 							validation: {
 								pattern: '[\+]\d{2}[\(]\d{2}[\)]\d{4}[\-]\d{4}'
 							}
 						},
-						{
+						/*{
 							id: 'homePhoneExtension',
 							name: 'homePhoneExtension',
 							label: 'ext.',
@@ -77,11 +77,11 @@ define({
 							data: {
 								bind: 'homePhoneExtension'
 							}
-						}
+						}*/
 					]
 				}
 			},
-			{
+			/*{
 				block: 'autorow',
 				config: {
 					items: [
@@ -109,12 +109,12 @@ define({
 						}
 					]
 				}
-			},
+			},*/
 			{
 				block: 'autorow',
 				config: {
 					items: [
-						{
+						/*{
 							id: 'cellPhone',
 							name: 'cellPhone',
 							label: 'Cell',
@@ -124,7 +124,7 @@ define({
 							data: {
 								bind: 'cellPhone'
 							}
-						},
+						},*/
 						{
 							id: 'faxNumber',
 							name: 'faxNumber',
@@ -133,207 +133,11 @@ define({
 							type: 'text',
 							class: 'medium k-textbox',
 							data: {
-								bind: 'faxNumber'
+								bind: 'fax'
 								// TODO: setLayout + config to set bindings
 								// That should be done with *some* of the other blocks too
 							}
 						}
-					]
-				}
-			},
-			{
-
-				tag: 'div',
-				class: 'kpaf-row field',
-				fields: [
-					{
-						tag: 'div',
-						class: 'fieldgroup',
-						group: [{
-							id: 'birthDate',
-							name: 'birthDate',
-							label: 'Birth Date',
-							tag: 'input',
-							type: 'date',
-							class: 'medium',
-							style: 'width: 208px',
-							data: {
-								role: 'datepicker',
-								bind: 'birthDate',
-								culture: 'en-CA',
-								format: 'dd/MM/yyyy',
-								parseFormats: ['yyyy-MM-dd', 'dd/MM/yyyy', 'd/MM/yyyy', 'd/M/yyyy']
-							},
-							validation: {
-								required: true,
-								date: true,
-								message: 'Please enter a valid date of birth (dd/mm/yyyy)'
-							}
-						}]
-					},
-					{
-						tag: 'div',
-						class: 'fieldgroup',
-						group: [{
-							id: 'gender',
-							name: 'gender',
-							label: 'Gender',
-							tag: 'input',
-							type: 'text',
-							class: 'medium',
-							style: 'width: 208px',
-							data: {
-								role: 'dropdownlist',
-								bind: {
-									source: {
-										type: 'custom',
-										config: {
-											data: [
-												{ Key: 'Male', Value: 'Male' },
-												{ Key: 'Female', Value: 'Female' }
-											]
-										}
-									},
-									value: 'gender'																		
-								},
-								optionLabel: ' '
-							},
-							validation: {
-								required: true,
-								message: 'Please choose a gender'
-							}
-						}]
-					},
-					// TODO: Add config to toggle field display
-					/*{
-						tag: 'div',
-						class: 'fieldgroup',
-						group: [{
-							id: 'estate',
-							name: 'estate',
-							label: 'Estate',
-							tag: 'input',
-							type: 'checkbox',
-							data: {
-								bind: {
-									checked: 'estate'
-								}
-							}
-						}]
-					}*/
-				]
-			},
-			{
-				block: 'autorow',
-				config: {
-					items: [
-						{
-							id: 'maritalStatus',
-							name: 'maritalStatus',
-							label: 'Marital Status',
-							tag: 'input',
-							type: 'text',
-							class: 'medium',
-							style: 'width: 208px',
-							data: {
-								role: 'dropdownlist',
-								bind: {
-									source: {
-										type: 'custom',
-										config: {
-											data: [
-												{ Key: 'Divorced', Value: 'Divorced' },
-												{ Key: 'Interdependent', Value: 'Interdependent' },
-												{ Key: 'Married', Value: 'Married' },
-												{ Key: 'Separated', Value: 'Separated' },
-												{ Key: 'Single', Value: 'Single' },
-												{ Key: 'Widowed', Value: 'Widowed' }
-											]
-										}
-									},
-									value: 'maritalStatus'
-								},
-								optionLabel: ' ',
-								validation: {
-									required: true,
-									message: 'Please choose a marital status'
-								}
-							}
-						},
-						{
-							id: 'occupation',
-							name: 'occupation',
-							label: 'Occupation',
-							tag: 'input',
-							type: 'text',
-							class: 'medium',
-							style: 'width: 308px',
-							data: {
-								role: 'combobox',
-								bind: {
-									source: {
-										type: 'custom',
-										config: {
-											data: [
-												{ Key: '169', Value: 'Accountant' },
-												{ Key: '161', Value: 'Accountant - CA Designation' },
-												{ Key: '163', Value: 'Accountant - CGA Designation' },
-												{ Key: '162', Value: 'Accountant - CMA Designation' },
-												{ Key: '120', Value: 'Architect' },
-												{ Key: '121', Value: 'Auto Repair Garage/Body Shop' },
-												{ Key: '300', Value: 'Bookkeeper' },
-												{ Key: '130', Value: 'Business - Professional' },
-												{ Key: '129', Value: 'Business Owner' },
-												{ Key: '140', Value: 'Child Care' },
-												{ Key: '150', Value: 'Computer Services' },
-												{ Key: '125', Value: 'Contractor' },
-												{ Key: '170', Value: 'Driver - Delivery/Truck/Taxi/Bus' },
-												{ Key: '189', Value: 'Education' },
-												{ Key: '182', Value: 'Education - Professor' },
-												{ Key: '181', Value: 'Education - Teacher' },
-												{ Key: '190', Value: 'Engineer - Professional' },
-												{ Key: '200', Value: 'Farmer' },
-												{ Key: '210', Value: 'Food Services/Hospitality' },
-												{ Key: '124', Value: 'Home Business' },
-												{ Key: '220', Value: 'Homemaker/Housewife' },
-												{ Key: '230', Value: 'Insurance/Financual' },
-												{ Key: '240', Value: 'Janitorial' },
-												{ Key: '250', Value: 'Laborer' },
-												{ Key: '260', Value: 'Lawyer' },
-												{ Key: '270', Value: 'Manager - Financial' },
-												{ Key: '280', Value: 'Manager - Non-financial' },
-												{ Key: '299', Value: 'Medical' },
-												{ Key: '294', Value: 'Medical - Dentist' },
-												{ Key: '292', Value: 'Medical - General Practitioner (GP)' },
-												{ Key: '296', Value: 'Medical - Nurse' },
-												{ Key: '295', Value: 'Medical - Pharmacist' },
-												{ Key: '293', Value: 'Medical - Surgeon' },
-												{ Key: '291', Value: 'Medical - Veterinarian' },
-												{ Key: '310', Value: 'Office Administrator' },
-												{ Key: '320', Value: 'Police/Fire/Ambulance/Military' },
-												{ Key: '330', Value: 'Real Estate' },
-												{ Key: '122', Value: 'Realty & Rental Property' },
-												{ Key: '123', Value: 'Rental & Service Stores' },
-												{ Key: '340', Value: 'Retired' },
-												{ Key: '350', Value: 'Sales/Retail' },
-												{ Key: '360', Value: 'Social WOrker' },
-												{ Key: '370', Value: 'Student' },
-												{ Key: '389', Value: 'Tradesperson' },
-												{ Key: '540', Value: 'Unemployed' }
-											]
-										}
-									},
-									value: 'occupation'
-								},
-								suggest: true,
-								filter: 'contains',
-								optionLabel: ' ',
-							},
-							validation: {
-								message: 'Please choose an occupation'
-							}
-						}
-					
 					]
 				}
 			}
--- blocks/block.personalinfo.js
+++ blocks/block.personalinfo.js
@@ -97,11 +97,11 @@ define({
 							type: 'text',
 							class: 'medium k-textbox',
 							data: {
-								bind: 'firstName'
+								bind: 'firstname'
 							}
 						}]
 					},
-					{
+					/*{
 						tag: 'div',
 						class: 'fieldgroup',
 						group: [{
@@ -115,7 +115,7 @@ define({
 								bind: 'middleName'
 							}
 						}]
-					},
+					},*/
 					{
 						tag: 'div',
 						class: 'fieldgroup',
@@ -127,11 +127,207 @@ define({
 							type: 'text',
 							class: 'medium k-textbox',
 							data: {
-								bind: 'lastName'
+								bind: 'lastname'
 							}
 						}]
 					}
 				]
+			},
+			{
+
+				tag: 'div',
+				class: 'kpaf-row field',
+				fields: [
+					{
+						tag: 'div',
+						class: 'fieldgroup',
+						group: [{
+							id: 'birthDate',
+							name: 'birthDate',
+							label: 'Birth Date',
+							tag: 'input',
+							type: 'date',
+							class: 'medium',
+							style: 'width: 208px',
+							data: {
+								role: 'datepicker',
+								bind: 'birthDate',
+								culture: 'en-CA',
+								format: 'dd/MM/yyyy',
+								parseFormats: ['yyyy-MM-dd', 'dd/MM/yyyy', 'd/MM/yyyy', 'd/M/yyyy']
+							},
+							validation: {
+								required: true,
+								date: true,
+								message: 'Please enter a valid date of birth (dd/mm/yyyy)'
+							}
+						}]
+					},
+					{
+						tag: 'div',
+						class: 'fieldgroup',
+						group: [{
+							id: 'gender',
+							name: 'gender',
+							label: 'Gender',
+							tag: 'input',
+							type: 'text',
+							class: 'medium',
+							style: 'width: 208px',
+							data: {
+								role: 'dropdownlist',
+								bind: {
+									source: {
+										type: 'custom',
+										config: {
+											data: [
+												{ Key: 'Male', Value: 'Male' },
+												{ Key: 'Female', Value: 'Female' }
+											]
+										}
+									},
+									value: 'gender'																		
+								},
+								optionLabel: ' '
+							},
+							validation: {
+								required: true,
+								message: 'Please choose a gender'
+							}
+						}]
+					},
+					// TODO: Add config to toggle field display
+					/*{
+						tag: 'div',
+						class: 'fieldgroup',
+						group: [{
+							id: 'estate',
+							name: 'estate',
+							label: 'Estate',
+							tag: 'input',
+							type: 'checkbox',
+							data: {
+								bind: {
+									checked: 'estate'
+								}
+							}
+						}]
+					}*/
+				]
+			},
+			{
+				block: 'autorow',
+				config: {
+					items: [
+						/*{
+							id: 'maritalStatus',
+							name: 'maritalStatus',
+							label: 'Marital Status',
+							tag: 'input',
+							type: 'text',
+							class: 'medium',
+							style: 'width: 208px',
+							data: {
+								role: 'dropdownlist',
+								bind: {
+									source: {
+										type: 'custom',
+										config: {
+											data: [
+												{ Key: 'Divorced', Value: 'Divorced' },
+												{ Key: 'Interdependent', Value: 'Interdependent' },
+												{ Key: 'Married', Value: 'Married' },
+												{ Key: 'Separated', Value: 'Separated' },
+												{ Key: 'Single', Value: 'Single' },
+												{ Key: 'Widowed', Value: 'Widowed' }
+											]
+										}
+									},
+									value: 'maritalStatus'
+								},
+								optionLabel: ' ',
+								validation: {
+									required: true,
+									message: 'Please choose a marital status'
+								}
+							}
+						},*/
+						/*{
+							id: 'occupation',
+							name: 'occupation',
+							label: 'Occupation',
+							tag: 'input',
+							type: 'text',
+							class: 'medium',
+							style: 'width: 308px',
+							data: {
+								role: 'combobox',
+								bind: {
+									source: {
+										type: 'custom',
+										config: {
+											data: [
+												{ Key: '169', Value: 'Accountant' },
+												{ Key: '161', Value: 'Accountant - CA Designation' },
+												{ Key: '163', Value: 'Accountant - CGA Designation' },
+												{ Key: '162', Value: 'Accountant - CMA Designation' },
+												{ Key: '120', Value: 'Architect' },
+												{ Key: '121', Value: 'Auto Repair Garage/Body Shop' },
+												{ Key: '300', Value: 'Bookkeeper' },
+												{ Key: '130', Value: 'Business - Professional' },
+												{ Key: '129', Value: 'Business Owner' },
+												{ Key: '140', Value: 'Child Care' },
+												{ Key: '150', Value: 'Computer Services' },
+												{ Key: '125', Value: 'Contractor' },
+												{ Key: '170', Value: 'Driver - Delivery/Truck/Taxi/Bus' },
+												{ Key: '189', Value: 'Education' },
+												{ Key: '182', Value: 'Education - Professor' },
+												{ Key: '181', Value: 'Education - Teacher' },
+												{ Key: '190', Value: 'Engineer - Professional' },
+												{ Key: '200', Value: 'Farmer' },
+												{ Key: '210', Value: 'Food Services/Hospitality' },
+												{ Key: '124', Value: 'Home Business' },
+												{ Key: '220', Value: 'Homemaker/Housewife' },
+												{ Key: '230', Value: 'Insurance/Financual' },
+												{ Key: '240', Value: 'Janitorial' },
+												{ Key: '250', Value: 'Laborer' },
+												{ Key: '260', Value: 'Lawyer' },
+												{ Key: '270', Value: 'Manager - Financial' },
+												{ Key: '280', Value: 'Manager - Non-financial' },
+												{ Key: '299', Value: 'Medical' },
+												{ Key: '294', Value: 'Medical - Dentist' },
+												{ Key: '292', Value: 'Medical - General Practitioner (GP)' },
+												{ Key: '296', Value: 'Medical - Nurse' },
+												{ Key: '295', Value: 'Medical - Pharmacist' },
+												{ Key: '293', Value: 'Medical - Surgeon' },
+												{ Key: '291', Value: 'Medical - Veterinarian' },
+												{ Key: '310', Value: 'Office Administrator' },
+												{ Key: '320', Value: 'Police/Fire/Ambulance/Military' },
+												{ Key: '330', Value: 'Real Estate' },
+												{ Key: '122', Value: 'Realty & Rental Property' },
+												{ Key: '123', Value: 'Rental & Service Stores' },
+												{ Key: '340', Value: 'Retired' },
+												{ Key: '350', Value: 'Sales/Retail' },
+												{ Key: '360', Value: 'Social WOrker' },
+												{ Key: '370', Value: 'Student' },
+												{ Key: '389', Value: 'Tradesperson' },
+												{ Key: '540', Value: 'Unemployed' }
+											]
+										}
+									},
+									value: 'occupation'
+								},
+								suggest: true,
+								filter: 'contains',
+								optionLabel: ' ',
+							},
+							validation: {
+								message: 'Please choose an occupation'
+							}
+						}*/
+					
+					]
+				}
 			}
 		]
 	}
